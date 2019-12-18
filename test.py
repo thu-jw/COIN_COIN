6611c1c20988fd51a1d15e9d6b65243ed0fd9fa2
wenliang zhao <zhaowl16@mails.tsinghua.edu.cn>
2019-12-19 00:25:59 +0800

    rename

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ length = []
 for key, info in tqdm(annotations['database'].items()):
     duration = info['duration']
     anno = info['annotation']
-    if len(anno) == 51:
-        embed()
-    length.append(len(anno))
+    for a in anno:
+        if int(a['id']) in [103, 228, 319, 381]:
+            print(a['id'], a['label'])
 
 # vim: ts=4 sw=4 sts=4 expandtab
